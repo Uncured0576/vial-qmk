b7de247043340bc7a965bb56bb7b78525f297fb2
Uncured0576 <kyleak96@gmail.com>
2025-04-05 15:42:12 +0800

    remap bootmagic to correct switch, update vial unlock to use left/right click

--- keyboards/ploopyco/trackball_thumb/keymaps/vial/config.h
+++ keyboards/ploopyco/trackball_thumb/keymaps/vial/config.h
@@ -5,4 +5,4 @@
 #define VIAL_KEYBOARD_UID {0xD1, 0x6A, 0xE9, 0x2A, 0xC4, 0xD0, 0x16, 0x35}
 
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 3, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 4 }
